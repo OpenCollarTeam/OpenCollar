OpenCollarTeam/OpenCollar

commit ca7c40cd58e4ff7cad4957c38dbbb22a8aa8b9f6

apply global_checkboxes in oc_core

src/collar/oc_core.lsl
@@ -620,6 +620,8 @@ default
                     g_iTouchNotify=(integer)sVal;
                 } else if(sVar == "allowhide"){
                     g_iAllowHide = (integer)sVal;
+                } else if(sVar == "checkboxes")
+                    g_lCheckboxes = llCSV2List(sVal);
                 }
             } else if(sToken == "auth"){
                 if(sVar == "group"){

commit a49887eee48673a0218b96db1be15eb0d68f5447

fix errors introduced by commits ca7c40cd and 5c273761

src/collar/oc_core.lsl
@@ -87,9 +87,9 @@ integer g_iAllowHide=TRUE;
 Settings(key kID, integer iAuth){
     string sPrompt = "OpenCollar\n\n[Settings]";
     list lButtons = ["Print", "Load", "Fix Menus"];
-    if (llGetInventoryType("oc_resizer") == INVENTORY_SCRIPT) lbuttons += ["Resize"];
-    else lbuttons += ["-"];
-    lbuttons += [Checkbox(g_iHide, "Hide"), "EDITOR", Checkbox(g_iAllowHide, "AllowHiding")];
+    if (llGetInventoryType("oc_resizer") == INVENTORY_SCRIPT) lButtons += ["Resize"];
+    else lButtons += ["-"];
+    lButtons += [Checkbox(g_iHide, "Hide"), "EDITOR", Checkbox(g_iAllowHide, "AllowHiding")];
     Dialog(kID, sPrompt, lButtons, [UPMENU],0,iAuth, "Menu~Settings");
 }
 
@@ -620,7 +620,7 @@ default
                     g_iTouchNotify=(integer)sVal;
                 } else if(sVar == "allowhide"){
                     g_iAllowHide = (integer)sVal;
-                } else if(sVar == "checkboxes")
+                } else if(sVar == "checkboxes"){
                     g_lCheckboxes = llCSV2List(sVal);
                 }
             } else if(sToken == "auth"){