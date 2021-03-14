2fd68a723f2200b6a98fab9e29c2629e86759bee
Lyseah <laura.lyseah@gmail.com>
2021-03-14 21:50:57 +0000

    meta(gha/lint): Add linting via lslint to GHA.

---
+++ run_lslint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+shopt -s globstar
+set -o pipefail
+
+LSLINT_PATH=$1
+FAILED=.
+
+for file in **/*.lsl; do
+    echo "[>>] Linting $file..." | tee -a ./test.run.txt
+    $1 "$file" 2>&1 | tee -a ./test.run.txt
+    
+    if [ ${PIPESTATUS[0]} != 0 ] ; then
+        echo "[!!] Linting failed on $file" | tee -a ./test.run.txt
+        FAILED=$FAILED.
+    else
+        echo "[>>] Linting passed" | tee -a ./test.run.txt
+    fi
+done
+
+if [ $FAILED != . ] ; then
+  echo "[!!!] Linting failed on ${#FAILED} files - check the logs."
+  exit ${#FAILED}
+fi
